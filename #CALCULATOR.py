1ecff87c745c96923045e420fab90d5a9d905ce8
Naveed Auliat <101603830+naveedauliat@users.noreply.github.com>
2025-02-26 18:45:28 +0500

    Update #CALCULATOR.py

--- #CALCULATOR.py
+++ #CALCULATOR.py
@@ -1,13 +1,23 @@
-class Point:
-    def move(self):
-        print("move")
-        
-    def draw(self):
-        print("draw")
-        
 
-point1 = Point()
-point1.x = 10
-point1.y = 20
-print(point1.x)
-point1.draw()
+print("""Select the option : 
+    1 : Add
+    2: Subtract
+    3: Multiply
+    4: Divide
+    """)
+
+c = int(input())    
+
+
+a = int(input("Enter 1st value : "))
+b = int(input("Enter 2nd value : "))
+
+if c == 1:
+    print(a, " + ", b, " is : ", a + b)
+elif c == 2:
+    print(a, " - ", b, " is : ",a - b)
+elif c == 3:
+    print(a, " x ", b, " is : ",a * b)
+elif c == 4:
+    print(a, " / ", b, " is : ",a / b) 
+
